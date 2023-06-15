b311e61c24cfd0379b88bddbc214d9ce42fd8bd8
suehiro tsuranuki <ts.ksc.wss@gmail.com>
2023-06-15 16:36:33 +0900

    レイアウトの変更

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,118 +1,130 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+const imageStyle = {
+  padding: "0px 12px",
+};
 
 export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
   subheading,
-  mainpitch,
-  description,
-  intro,
 }) => (
   <div>
+    <Carousel
+      showThumbs={false}
+      centerSlidePercentage={55}
+      autoPlay
+      showArrows
+      infiniteLoop
+      stopOnHover
+      centerMode
+    >
+      <div style={imageStyle}>
+        <img src="/img/1686808769079.jpg" alt="bread image" />
+      </div>
+      <div style={imageStyle}>
+        <img src="/img/1686808768862.jpg" alt="bread image" />
+      </div>
+      <div style={imageStyle}>
+        <img src="/img/1686808768968.jpg" alt="bread image" />
+      </div>
+      <div style={imageStyle}>
+        <img src="/img/1686808769023.jpg" alt="bread image" />
+      </div>
+    </Carousel>
     <div
       className="full-width-image margin-top-0"
       style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
         backgroundPosition: `top left`,
         backgroundAttachment: `fixed`,
       }}
     >
       <div
         style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
+          display: "flex",
+          height: "72px",
+          lineHeight: "1",
+          justifyContent: "space-around",
+          alignItems: "left",
+          flexDirection: "column",
         }}
       >
-        <h1
+        {/* <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+            backgroundColor: "rgb(255, 68, 0)",
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em",
           }}
         >
           {title}
-        </h1>
+        </h1> */}
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+            // boxShadow:
+              // "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+            // backgroundColor: "rgb(255, 68, 0)",
+            // color: "white",
+            lineHeight: "1",
+            padding: "0.25em",
           }}
         >
           {subheading}
         </h3>
       </div>
     </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
+    <div className="container">
+      <div className="column is-10 is-offset-1">
+        <div className="content">
+          {/* <div className="content">
+            <div className="tile">
+              <h1 className="title">{mainpitch.title}</h1>
+            </div>
+            <div className="tile">
+              <h3 className="subtitle">{mainpitch.description}</h3>
+            </div>
+          </div> */}
+          {/* <div className="columns">
+            <div className="column is-12">
+              <h3 className="has-text-weight-semibold is-size-2">
+                {heading}
+              </h3>
+              <p>{description}</p>
+            </div>
+          </div> */}
+          {/* <Features gridItems={intro.blurbs} /> */}
+          {/* <div className="columns">
+            <div className="column is-12 has-text-centered">
+              <Link className="btn" to="/products">
+                See all products
+              </Link>
+            </div>
+          </div> */}
+          <div className="column is-12">
+            <h3>
+              News
+            </h3>
+            <BlogRoll />
+            <div className="column is-12 has-text-centered">
+              <Link className="btn" to="/blog">
+                Read more
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   </div>
-)
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -124,10 +136,10 @@ IndexPageTemplate.propTypes = {
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
-}
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
@@ -141,8 +153,8 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -150,9 +162,9 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -190,4 +202,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
