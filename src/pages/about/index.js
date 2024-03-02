e755232a1cb3e3b9082d80b51b8ad217de71d629
SuehiroTsuranuki <ts.ksc.wss@gmail.com>
2024-03-02 10:06:38 +0900

    update calendar

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -9,7 +9,7 @@ export default function Access() {
         <div className="container content">
           <div>
             <h3>営業日</h3>
-            <img src="/img/1707266709015.jpg" alt="2024年 2月の営業日" />
+            <img src="/img/1709341180198.jpg" alt="2024年 3月の営業日" />
           </div>
         </div>
       </section>
